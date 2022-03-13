8efcee160f26698751e5cfd981ed09bb753f3056
Nomii <naumanmajeed7@gmail.com>
2022-03-13 13:56:58 +0500

    first try

--- app/src/main/java/com/bettingtipsking/app/ui/account/NotificationActivity.java
+++ app/src/main/java/com/bettingtipsking/app/ui/account/NotificationActivity.java
@@ -13,5 +13,6 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
     }
 }
